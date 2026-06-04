8fd5b09d9f91163780ae8a52ed7ca33e1aea08d8
Weder Hovadich Gonçalves <weder.goncalves@sga.pucminas.br>
2026-06-04 19:56:10 -0300

    correção pop-up tela outras receitas; correção cálculo IMC; inclusãode botão imprimir na tela agenda

--- befit/IMC/script_imc.js
+++ befit/IMC/script_imc.js
@@ -19,10 +19,10 @@ function calcularIMC() {
 	// Função para classificar o IMC
 	function classificarIMC(imc) {
     if (imc < 18.5) return "Abaixo do peso";
-    if (imc < 24.9) return "Peso normal";
-    if (imc < 29.9) return "Sobrepeso";
-    if (imc < 34.9) return "Obesidade Grau 1";
-    if (imc < 39.9) return "Obesidade Grau 2";
+    if (imc <= 24.9) return "Peso normal";
+    if (imc <= 29.9) return "Sobrepeso";
+    if (imc <= 34.9) return "Obesidade Grau 1";
+    if (imc <= 39.9) return "Obesidade Grau 2";
     if (imc >= 40) return "Obesidade Grau 3";
 	}
 		
